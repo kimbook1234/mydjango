d42ad140e109aca08828e88f2184b566f6fe80b6
book kim <kimjjw2018@gmail.com>
2025-04-04 22:56:07 +0900

    데이터베이스 적용

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -4,3 +4,14 @@ ALLOWED_HOSTS = ['52.79.152.19']
 STATIC_ROOT = BASE_DIR / 'static/'
 STATICFILES_DIRS = []
 DEBUG = False
+
+DATABASE = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'pybo',
+        'USER': 'dbmasteruser',
+        'PASSWORD': '|EU0Xyce:Uvt*%]z$wLHXn+t`K?>>45A',
+        'HOST': 'ls-ec4ed14b345ac1a2d7c87f9f8542f2356ada415e.cr2ic42mkx8y.ap-northeast-2.rds.amazonaws.com',
+        'PORT': '5432',
+    }
+}
